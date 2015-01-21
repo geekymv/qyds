17555d1308026c2c35bcf19c301ba78b49297288
zdxu <418977907@qq.com>
2015-01-21 19:53:31 +0800

    add file

---
+++ test/com/gos/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,14 @@
+package com.gos.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
